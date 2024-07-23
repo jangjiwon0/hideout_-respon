bae2c398c4b4c669e0bc8786e1a42eb295860e6c
jangjiwon <jw027878@gmail.com>
2024-07-23 13:02:30 +0900

    Add files via upload

--- js/script_index.js
+++ js/script_index.js
@@ -2,7 +2,7 @@ $( document ).ready( function() {
         var jbOffset = $( '.header_inner' ).offset();
         /*선택자는 본인 구조에 맞게 수정*/
         $( window ).scroll( function() {
-          if ( $( document ).scrollTop() > 903) {//여기 지정된 길이 이후부터 적용됩니다
+          if ( $( document ).scrollTop() >628) {//여기 지정된 길이 이후부터 적용됩니다
             $( '.header_inner' ).addClass("on");
             /*선택자는 본인 구조에 맞게 수정, on클래스 만들어서 변경 원하는 스타일 주세요*/
           }
